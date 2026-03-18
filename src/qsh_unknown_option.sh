1fdb05966e867bd5ceb71381b804aed1ea8e93f1
Quinn Grier <quinn@quinngrier.com>
2026-03-17 17:39:24 -0700

    Add qsh_unknown_option

---
+++ src/qsh_unknown_option.sh
@@ -0,0 +1,31 @@
+#
+# The authors of this file have waived all copyright and
+# related or neighboring rights to the extent permitted by
+# law as described by the CC0 1.0 Universal Public Domain
+# Dedication. You should have received a copy of the full
+# dedication along with this file, typically as a file
+# named <CC0-1.0.txt>. If not, it may be available at
+# <https://creativecommons.org/publicdomain/zero/1.0/>.
+#
+
+qsh_unknown_option() {
+
+  case ${QSH_NDEBUG+1} in ("")
+    case $# in (1)
+      :
+    ;; (*)
+      qsh_barf "qsh_unknown_option:" \
+        "Exactly one argument must be given" \
+      ;
+    esac
+  esac
+
+  case $1 in (--*)
+    qsh_barf "Unknown option: \"${1%%=*}\""
+  ;; (-?*)
+    qsh_unknown_option_tmp=${1#??}
+    qsh_unknown_option_tmp=${1%"$qsh_unknown_option_tmp"}
+    qsh_barf "Unknown option: \"$qsh_unknown_option_tmp\""
+  esac
+
+}
