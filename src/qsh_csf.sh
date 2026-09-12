a94d9723a20d66fc45c46de5c5b5fdc3627a1f22
Quinn Grier <quinn@quinngrier.com>
2026-09-12 00:51:01 -0700

    Simplify the copyright notices

--- src/qsh_csf.sh
+++ src/qsh_csf.sh
@@ -1,11 +1,5 @@
 #
-# The authors of this file have waived all copyright and
-# related or neighboring rights to the extent permitted by
-# law as described by the CC0 1.0 Universal Public Domain
-# Dedication. You should have received a copy of the full
-# dedication along with this file, typically as a file
-# named <CC0-1.0.txt>. If not, it may be available at
-# <https://creativecommons.org/publicdomain/zero/1.0/>.
+# This file is published under the CC0 Public Domain Dedication.
 #
 
 qsh_csf() {
